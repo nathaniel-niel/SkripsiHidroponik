774f21bdc0c95e509c6b3badf3b2f6f43ca3332b
Nathaniel Andrian <nathaniel.task@gmail.com>
2021-12-16 21:31:32 +0700

    fixing response

--- arduino/skripsi-project/nodemcu/nodemcu.ino
+++ arduino/skripsi-project/nodemcu/nodemcu.ino
@@ -3,8 +3,8 @@
 #include <WiFiClient.h>
 
 // Connection stuffs
-const char* ssid = "Hello World";
-const char* pass = "88888888";
+const char* ssid = "Alberthome_ext";
+const char* pass = "basketball29";
 
 // Delay Configuration
 unsigned long lastTime = 0;
@@ -22,6 +22,7 @@ WiFiClient client;
 // Variables
 String data;
 int incomingByte = 0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -76,7 +77,7 @@ void sendData(){
 //   Start HTTP Connection
 //  if (http.begin(client, "http://192.168.1.13/SkripsiHidroponik/arduino/phpfile/data.php?"+data)){
   // Start connection and send HTTP header
-  if (http.begin(client, "http://192.168.1.11/SkripsiHidroponik/arduino/phpfile/data.php?device_id=DVC002&sensor_ph=8&sensor_ppm=500&sensor_level_air=LOW")){
+  if (http.begin(client, "http://192.168.100.125/SkripsiHidroponik/arduino/phpfile/data.php?device_id=DVC001&sensor_ph=8&sensor_ppm=500&sensor_level_air=LOW")){
    
     int httpCode = http.GET();
      if (httpCode > 0){
@@ -86,35 +87,64 @@ void sendData(){
       if (httpCode == HTTP_CODE_OK || HTTP_CODE_MOVED_PERMANENTLY){
         String payload = http.getString();
         Serial.println(payload);
-
-        if (payload == "add pH down"){
+     
+        float value_ph = 0;
+        int value_pm = 0;
+        String code_ph_raw, code_pm_raw, code_ph, code_pm;
+        String response;
+        code_ph_raw = payload.substring(0,payload.indexOf(" "));
+        code_pm_raw = payload.substring(payload.indexOf(" ")+1);
+        Serial.println("RAW VALUE");
+        Serial.println("ppm: " + code_pm_raw);
+        Serial.println("ph: " + code_ph_raw);
+        value_ph = code_ph_raw.substring(code_ph_raw.indexOf("_")+1).toFloat();
+        value_pm = code_pm_raw.substring(code_pm_raw.indexOf("_")+1).toFloat();
+        Serial.println("ph and ppm value");
+        Serial.println("value ph: " +String(value_ph));
+        Serial.println("value pm: "+String(value_pm));
+        Serial.println("response code");
+        code_ph =  code_ph_raw.substring(0,code_ph_raw.indexOf("_"));
+        code_pm = code_pm_raw.substring(0,code_pm_raw.indexOf("_"));
+        Serial.println("code ph: " +code_ph);
+        Serial.println("code pm: "+code_pm);
+        response = code_ph+code_pm;
+        Serial.println("response: " + response);
+        
+        
+      
+        if (response == "pd"){
             setPinRate(relay_ph_down, HIGH);
             delay(2000);
+            Serial.println("pd OK!");
             setPinRate(relay_ph_down, LOW);
         }
-        else if(payload == "add pH up"){
+        else if(response == "pu"){
             setPinRate(relay_ph_up, HIGH);
             delay(2000);
             setPinRate(relay_ph_up, LOW);
+            Serial.println("pu OK!");
         }
-        else if(payload == "add ppm"){
+        else if(response == "pm"){
             setPinRate(relay_ppm, HIGH);
             delay(2000);
             setPinRate(relay_ppm, LOW);
+            Serial.println("pm OK!");
         }
-        else if(payload == "add pH downadd ppm"){
+        else if(response == "pdpm"){
             setPinRate(relay_ph_down, HIGH);
             setPinRate(relay_ppm, HIGH);
             delay(2000);
             setPinRate(relay_ph_down, LOW);
             setPinRate(relay_ppm, LOW);
+            Serial.println("pdpm OK!");
         }
-        else if(payload == "add pH upadd ppm"){
+        else if(response == "pupm" ){
             setPinRate(relay_ph_up, HIGH);
             setPinRate(relay_ppm, HIGH);
             delay(2000);
             setPinRate(relay_ph_up, LOW);
             setPinRate(relay_ppm, LOW);
+            Serial.println("pupm OK!");
         }
       }
     }else{
